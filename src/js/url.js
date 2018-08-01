2097d71d2c43b1bfe54eef44f2f9385336572e9d
pengfei.yang <pengfei.yang@rokid.com>
2018-08-01 19:32:11 +0800

    feat: 最终线上地址版

--- src/js/url.js
+++ src/js/url.js
@@ -1,6 +1,6 @@
 //本地调试地址
-export const devUrl = ""
+// export const devUrl = ""
 //测试地址
 // export const devUrl = "https://cas-operate-activity-test.rokid.com/rokid-cas-activity"
 //线上地址
-// export const devUrl = "https://cas-operate-activity-pro.rokid.com/rokid-cas-activity"
+export const devUrl = "https://cas-operate-activity-pro.rokid.com/rokid-cas-activity"
